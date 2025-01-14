fed4c762be80185525d079b53443358d9b0bc923
Mendel Hoffman <mmhoffman24@gmail.com>
2025-01-14 13:32:36 +1100

    commit custom domain Tue

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import autoprefixer from 'autoprefixer'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/porfolio-website/',
-  css: {  
+  base: "/porfolio-website/",
+  css: {
     postcss: {
       plugins: [
         tailwindcss,
